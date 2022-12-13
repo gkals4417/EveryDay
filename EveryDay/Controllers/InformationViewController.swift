d01e21f253ad61241f920a364a7062af52afdd53
gkals4417 <gkals4417@gmail.com>
2022-12-13 15:54:37 +0900

    feat : minor update

--- EveryDay/Controllers/InformationViewController.swift
+++ EveryDay/Controllers/InformationViewController.swift
@@ -6,11 +6,14 @@
 //
 
 import UIKit
-
+import HGCircularSlider
 class InformationViewController: UIViewController {
 
+    @IBOutlet weak var secondView: UIView!
+    @IBOutlet weak var thirdView: UIView!
+    @IBOutlet weak var fourthView: UIView!
+    @IBOutlet weak var progress: CircularSlider!
     
-    @IBOutlet weak var collectionView: UICollectionView!
     let appManager = EveryDayManager.shared
     let sectionInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
     var receivedArray: [Int] = [] {
@@ -23,43 +26,56 @@ class InformationViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         initialFunc()
-        collectionView.delegate = self
-        collectionView.dataSource = self
+        appearanceFunc()
         view.backgroundColor = .white
     }
     
-    private func initialFunc(){
+    private func initialFunc() {
         guard let temp = appManager.delegate?.getInfo() else { return }
         receivedArray = temp
+    }
+    
+    private func appearanceFunc() {
+        progress.layer.backgroundColor = CGColor(red: 222/255, green: 222/255, blue: 222/255, alpha: 0.5)
+        progress.layer.cornerRadius = 12
+        progress.layer.masksToBounds = true
+        progress.layer.borderWidth = 0
+        progress.layer.borderColor = CGColor(red: 0, green: 0, blue: 0, alpha: 0.5)
         
+        secondView.layer.backgroundColor = CGColor(red: 222/255, green: 222/255, blue: 222/255, alpha: 0.5)
+        secondView.layer.cornerRadius = 12
+        secondView.layer.masksToBounds = true
+        secondView.layer.borderWidth = 0.2
+        secondView.layer.borderColor = CGColor(red: 0, green: 0, blue: 0, alpha: 0.5)
         
+        thirdView.layer.backgroundColor = CGColor(red: 222/255, green: 222/255, blue: 222/255, alpha: 0.5)
+        thirdView.layer.cornerRadius = 12
+        thirdView.layer.masksToBounds = true
+        thirdView.layer.borderWidth = 0.2
+        thirdView.layer.borderColor = CGColor(red: 0, green: 0, blue: 0, alpha: 0.5)
+        
+        fourthView.layer.backgroundColor = CGColor(red: 222/255, green: 222/255, blue: 222/255, alpha: 0.5)
+        fourthView.layer.cornerRadius = 12
+        fourthView.layer.masksToBounds = true
+        fourthView.layer.borderWidth = 0.2
+        fourthView.layer.borderColor = CGColor(red: 0, green: 0, blue: 0, alpha: 0.5)
+        
+        progress.backgroundColor = .clear
+        progress.diskColor = .clear
+        progress.trackColor = .clear
+        progress.trackFillColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1.0)
+        progress.backtrackLineWidth = 20
+        progress.lineWidth = 20
+        progress.thumbRadius = 10
+        progress.thumbLineWidth = 0
+        progress.endThumbTintColor = .clear
+        progress.minimumValue = 0
+        progress.maximumValue = 1
+//        progress.diskFillColor = UIColor(red: 222/255, green: 222/255, blue: 222/255, alpha: 0.5)
+        progress.isUserInteractionEnabled = false
+        progress.endPointValue = 0.5
     }
     
 }
 
-extension InformationViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 4
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "InfoCell", for: indexPath) as! InfoCell
-        cell.testLabel.text = menuArray[indexPath.row]
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-            let width = collectionView.frame.width
-            let height = collectionView.frame.height
-            let itemsPerRow: CGFloat = 2
-            let widthPadding = sectionInsets.left * (itemsPerRow - 1)
-            let itemsPerColumn: CGFloat = 2
-            let heightPadding = sectionInsets.top * (itemsPerColumn + 1)
-            let cellWidth = (width - widthPadding) / itemsPerRow
-            let cellHeight = (height - heightPadding) / itemsPerColumn
-            
-            return CGSize(width: cellWidth, height: cellHeight)
-            
-        }
-    
-}
+
